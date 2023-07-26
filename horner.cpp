808664c4546e3e5eff2a947b0b849bebcd948add
szekeres <59123877+szekeres3@users.noreply.github.com>
2023-07-26 12:46:22 +0200

    Update horner.cpp

--- horner.cpp
+++ horner.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <array>
 #include <cmath>
-int Horner(int* e, int k, int n){
+int Horner(int* e, int k, int n, int* eh){
     int eredmeny = e[n];
-    for (size_t i = n-1; i > 0; i--)
+    int i;
+    for (i = n-1; i > 0; i--)
     {
+        eh[n-i-1] = eredmeny;
         eredmeny = k * eredmeny + e[i];
-        //std::cout << eredmeny << std::endl;
-        //std::cin.get();
     }
+    eh[n-i-1] = eredmeny;
     return k * eredmeny + e[0]; 
+}
+void kiir(int kszi, int h, int* x, int n){
+std::cout << "f(" << kszi << ") = " << h << " és" << std::endl 
+    << "f(x) = (x-" << kszi << ")(";
+    for (int i = 0; i<n; i++)
+    {
+        if (x[i] < 0)
+        {
+            std::cout << " - ";
+        }
+        std::cout << abs(x[i]);
+        std::cout << "x^" << n-i-1;
+        if (i == n-1)
+        {
+            std::cout << ")";
+        }
+        if (x[i+1] > -1)
+        {
+            std::cout << " + ";
+        }
+    }
+    std::cout << h << std::endl;
 }
 int main(){
     int n;
@@ -21,38 +44,15 @@ int main(){
         std::exit(1);
     }
     int egyutthatok[n+1];
+    int * x = new int[n];
     int kszi;
-    std::cout << "kszí megadása" << std::endl;
+    std::cout << "kszí megadása: ";
     std::cin >> kszi;
-    for (size_t i = 0; i <= n; i+=1)
+    for (size_t i = 0; i <= n; i++)
     {
-        std::cout << "Kérem a számot: " << i << std::endl;
+        std::cout << "Kérem a számot (x^" << i << "): ";
         std::cin >> egyutthatok[i];
     }
-    int h = Horner(egyutthatok, kszi, n);
-    std::cout << "f(" << kszi << ") = " << h << " és" << std::endl 
-    << "f(x) = (x-" << kszi << ")(";
-    int index = 0;
-    for (int elem: egyutthatok)
-    {
-        if (elem < 0)
-        {
-            std::cout << " - ";
-        }
-        std::cout << abs(elem);
-        if (index > 0)
-        {
-            std::cout << "x^" << index;
-        }
-        if (index == n)
-        {
-            std::cout << ")";
-        }
-        if (egyutthatok[index+1] > -1)
-        {
-            std::cout << " + ";
-        }
-        index++;
-    }
-    std::cout << h << std::endl;
+    int h = Horner(egyutthatok, kszi, n, x);
+    kiir(kszi, h, x, n);
 }
